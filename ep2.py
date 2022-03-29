15c6b2ff405ea5ef2b4a92125cdca9f809c08560
ploylapas <lapasrada_k@cmu.ac.th>
2022-03-30 02:01:54 +0700

    add ep2.py

---
+++ ep2.py
@@ -0,0 +1,29 @@
+print("Hello PP")
+name = "PP"
+lastname = "salmon"
+fullname = name + ' ' + lastname
+fullname1 =  '{} {}'.format(name, lastname)
+print(fullname1)
+fullname2 = 'My name is {} and my lastname is {}'.format(name, lastname)
+print(fullname2)
+print(f'Name: {name} Lastname: {lastname}')
+print("-------------------")
+age = 22
+grade = 3.59
+print(type(age))
+print(type(grade))
+print(type(name))
+print(f'age: {age} grade: {grade :5f}')
+print(int(grade))
+print(round(3.3456))
+print(round(3.5567))
+print("10/3= ", 10/3)
+print("10//3= ", 10//3)
+print("10%3= ", 10%3)
+print("25/6= ", 25/6)
+print("25//6= ", 25//6)
+print("25%6= ", 25%6)
+print("3/2= ", 3/2)
+print("3//2= ", 3//2)
+print("3%2= ", 3%2)
+
