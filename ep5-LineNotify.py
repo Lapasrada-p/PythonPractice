dfa984d3ee5fd4a48c8a063766b7275e38929f21
ploylapas <lapasrada_k@cmu.ac.th>
2022-04-10 02:09:39 +0700

    add ep5

---
+++ ep5-LineNotify.py
@@ -0,0 +1,6 @@
+from songline import Sendline
+token = 'z4D6nqpPk8f9ZQ06hsBcYUhLUxhWQgBa17TfGKmvIUd'
+messenger = Sendline(token)
+messenger.sendtext('hello world')
+messenger.sticker(620,4)
+messenger.sendimage("")
